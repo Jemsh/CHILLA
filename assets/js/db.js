aefd99a47c728739d8e60d5c955c61826b2e941f
JEMSH <jemsh@JEMSHs-MacBook-Pro.local>
2021-05-27 21:24:59 +0530

    All content writing  has been finished. next need to change all icons and images

---
+++ assets/js/db.js
@@ -0,0 +1,33 @@
+var data = {
+    "english":
+    {
+        "title": "Opportunity to be a Chilla branch",
+
+        "titleTwo": "We are now offering our franchisees in Kerala",
+
+        "descriptionOne": "Everyone knows that the supermarket is a highly profitable business in any pandemic situation and its operations are always very difficult.We do all these difficult operations for you. We make good and right choices in every aspect of the business that will bring assured profit to you.We have acquired this art through experience and regular updates over 35 years; this gives us confidence.Do you want a regular monthly income without knowing any hassle or difficulty of this business, be a part of the prestigious Chilla Supermarket Chains!The rest is our guarantee!",
+
+        "titleBenifits": "BENEFITS OF CHILLA RETAIL FRANCHISEE",
+
+        "benifitsHeadOne": "SOP In Whole Area",
+        "benifitPointOne": "We retain the Standard Of Procedures in every area.We build and furnish the store.",
+
+        "benifitsHeadTwo": "Team Management",
+        "benifitPointTwo": "We recruit and place the team as your store requires.We train the team in customer behaviour, sales, products, billing, merchandising, etc/.To increase the sales, we assign targets and commissions to the team",
+
+        "benifitsHeadThree": "Stock Management System",
+        "benifitPointThree": "We purchase stock. Replenishing stock as and when required. Pricing Stock. We purchase goods from companies with special prices",
+
+        "benifitsHeadFoure": "Marketing",
+        "benifitPointFoure": "We study your market regularly.We spend a certain amount on marketing for each store every month. To achieve our sales target, we develop a suitable strategy for your location",
+
+        "benifitsHeadFive": "Private Labeling ",
+        "benifitPointFive": "We produce products under our brand name. In this way, we ensure quality and price",
+        
+        "benifitsHeadSix": "Customer Relationship",
+        "benifitPointSix": "Our purchasing system gives the customer large amounts of discount. Customer loyalty packages. Customer relationship management system"
+
+
+
+    }
+}
